d933d4b4a35a3bf938121eb19433cfd841419cee
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-07-16 10:32:15 -0600

    opcion 1: datos del estudiante

--- actividad_7.py
+++ actividad_7.py
@@ -8,3 +8,12 @@ while opcion!=4 :
     opcion = int(input("Elija una opción: "))
     if opcion <= 0 or opcion >4 :
         print("Ingreso un dato incorrecto")
+    elif opcion == 1 :
+        cantidad = int(input("¿Cuántos estudiantes desea ingresar? "))
+        for i in range(cantidad) :
+            print(f"\nEstudiante {i+1}")
+            carne = int(input("Ingrese el carné: "))
+            nombre = input("Ingrese el nombre completo: ")
+            edad = int(input("Ingrese la edad: "))
+            carrera = input("Ingrese la carrera: ")
+            print(f"\nIngrese la cantidad de cursos que desea registrar: ")
